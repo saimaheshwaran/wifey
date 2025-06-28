0ae99e8fa5a49bfca169d053b7c7fe70dca05e42
saimaheshwaran <saimaheshwaran@outlook.com>
2025-06-28 14:49:26 -0400

    code updated

--- src/main/java/com/sm/wifey/repository/BlogCommentRepository.java
+++ src/main/java/com/sm/wifey/repository/BlogCommentRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public interface BlogCommentRepository extends JpaRepository<BlogComment, Long> {
 
-    @Query(value = "select id, author_name, content, created_at, show_enabled, post_id, parent_comment_id from blog_comment order by created_at desc", nativeQuery = true)
+    @Query(value = "select author_name, content, created_at, show_enabled, post_id, parent_comment_id from blog_comment order by created_at desc", nativeQuery = true)
     List<Contact> getAllByLatestDate();
 
 }
