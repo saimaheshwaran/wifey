551d12c3d8b5ea205ebd0c6f28f5ae20d37869c3
saimaheshwaran <saimaheshwaran@outlook.com>
2025-06-28 15:12:43 -0400

    fixed repos

--- src/main/java/com/sm/wifey/repository/ContactRepository.java
+++ src/main/java/com/sm/wifey/repository/ContactRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Repository
 public interface ContactRepository extends JpaRepository<Contact, Long> {
 
-    @Query(value = "select id, name, subject, message, telephone, email, created_at from contact order by created_at desc", nativeQuery = true)
+    @Query(value = "select * from contact order by created_at desc", nativeQuery = true)
     List<Contact> getAllByLatestDate();
 
 }
